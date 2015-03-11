f135224a5111e97e8ae57e08c67d37cd4de7de00
Wiland <wiland2315@gmail.com>
2015-03-10 19:18:28 -0500

    Se agrega bootstrap

--- Malla_Turnos/PAGES/index.php
+++ Malla_Turnos/PAGES/index.php
@@ -1,7 +1,20 @@
+<?php ?>
 <html>
 <head>
-<title></title>
+<link type="text/css" href="../STYLES/bootstrap.min.css" />
+<link type="text/css" href="../STYLES/bootstrap-theme.min.css" />
+<script type="text/javascript" src="../SCRIPTS/bootstrap.min.js" />
+<title>Malla de Turnos</title>
 </head>
 <body>
+	<div class="row">
+		<div class="col-md-8">.col-md-8</div>
+		<div class="col-md-4">.col-md-4</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">.col-md-4</div>
+		<div class="col-md-4">.col-md-4</div>
+		<div class="col-md-4">.col-md-4</div>
+	</div>
 </body>
 </html>
