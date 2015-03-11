6df5bc8d48e579b08d4dfa0f02c9416bff41fedc
Wiland <wiland2315@gmail.com>
2015-03-10 20:59:10 -0500

    Se actualizan estilos

--- Malla_Turnos/PAGES/index.php
+++ Malla_Turnos/PAGES/index.php
@@ -1,20 +1,52 @@
-<?php ?>
-<html>
+<html lang="es-es">
 <head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta name="language" content="es-ES">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="expires" content="0">
 <link rel="stylesheet" href="../STYLES/bootstrap.min.css" />
 <link rel="stylesheet" href="../STYLES/bootstrap-theme.min.css" />
+<link rel="stylesheet" href="../STYLES/header.css" />
+<link rel="stylesheet" href="../STYLES/menu.css" />
+<script type="text/javascript" src="../SCRIPTS/jquery-1.11.2.min.js"></script>
 <script type="text/javascript" src="../SCRIPTS/bootstrap.min.js"></script>
+<script type="text/javascript" src="../SCRIPTS/menu.js"></script>
 <title>Malla de Turnos</title>
 </head>
 <body>
-	<div class="row">
-		<div class="col-md-8">.col-md-8</div>
-		<div class="col-md-4">.col-md-4</div>
+	<div class="row show-grid">
+		<div class="col-xs-6 col-sm-12">
+			<div id="header">
+				<div>
+					<a href="menu.php"><img src="../IMAGES/logo.png"></img></a>
+					<h1 class="title">Malla de turnos</h1>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-4">.col-md-4</div>
-		<div class="col-md-4">.col-md-4</div>
-		<div class="col-md-4">.col-md-4</div>
+
+	<div class="row show-grid">
+		<div class="col-xs-6 col-sm-3">
+			<div id="medium">
+				<div id="mediumContent">
+					<!-- Start SideBarMenu -->
+					<div id="sideBarMenu">
+						<h2>Menú</h2>
+						<ul id="menu">
+							<li><a target="contenido" href="pages/gUsuarios.php">Gestionar
+									Usuarios</a></li>
+							<li><a href="#" onclick="toggle()">Evaluacion Desempeño</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-6 col-sm-8">
+			<iframe name="contenido" style="width: 60%; height: 60%"></iframe>
+		</div>
 	</div>
+
+
 </body>
 </html>
