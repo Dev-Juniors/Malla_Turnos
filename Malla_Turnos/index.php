e4366873053848a11762f309d0d9321ea936efde
Wiland <wiland2315@gmail.com>
2015-03-13 20:18:48 -0500

    Se actualizan páginas

--- Malla_Turnos/index.php
+++ Malla_Turnos/index.php
@@ -4,13 +4,14 @@
 <meta name="language" content="es-ES">
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="expires" content="0">
-<link rel="stylesheet" href="../STYLES/bootstrap.min.css" />
-<link rel="stylesheet" href="../STYLES/bootstrap-theme.min.css" />
-<link rel="stylesheet" href="../STYLES/header.css" />
-<link rel="stylesheet" href="../STYLES/menu.css" />
-<script type="text/javascript" src="../SCRIPTS/jquery-1.11.2.min.js"></script>
-<script type="text/javascript" src="../SCRIPTS/bootstrap.min.js"></script>
-<script type="text/javascript" src="../SCRIPTS/menu.js"></script>
+<link rel="stylesheet" href="STYLES/bootstrap.min.css" />
+<link rel="stylesheet" href="STYLES/bootstrap-theme.min.css" />
+<link rel="stylesheet" href="STYLES/header.css" />
+<link rel="stylesheet" href="STYLES/menu.css" />
+<link rel="stylesheet" href="STYLES/index.css" />
+<script type="text/javascript" src="SCRIPTS/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="SCRIPTS/bootstrap.min.js"></script>
+<script type="text/javascript" src="SCRIPTS/menu.js"></script>
 <title>Malla de Turnos</title>
 </head>
 <body>
@@ -18,7 +19,7 @@
 		<div class="col-xs-6 col-sm-12">
 			<div id="header">
 				<div>
-					<a href="menu.php"><img src="../IMAGES/logo.png"></img></a>
+					<a href="menu.php"><img src="IMAGES/logo.png"></img></a>
 					<h1 class="title">Malla de turnos</h1>
 				</div>
 			</div>
@@ -35,7 +36,7 @@
 						<ul id="menu">
 							<li><a target="contenido" href="pages/gUsuarios.php">Gestionar
 									Usuarios</a></li>
-							<li><a href="#" onclick="toggle()">Evaluacion Desempeño</a></li>
+							<li><a target="contenido" href="PAGES/GenerarModelo.html">Generar Modelo</a></li>
 						</ul>
 					</div>
 				</div>
@@ -43,10 +44,8 @@
 		</div>
 
 		<div class="col-xs-6 col-sm-8">
-			<iframe name="contenido" style="width: 60%; height: 60%"></iframe>
+			<iframe id="contenido" name="contenido"></iframe>
 		</div>
 	</div>
-
-
 </body>
 </html>
