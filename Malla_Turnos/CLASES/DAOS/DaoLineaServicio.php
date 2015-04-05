34e55f21da9f5ede7d8b290da5ea6b96ff0ae44b
gabojrm <gabojrm@gmail.com>
2015-04-05 10:45:33 -0500

    Se actualiza DAO de lds

--- Malla_Turnos/CLASES/DAOS/DaoLineaServicio.php
+++ Malla_Turnos/CLASES/DAOS/DaoLineaServicio.php
@@ -7,18 +7,56 @@ class DaoLineaServicio {
 	public function DaoLineaServicio($lineaServicio) {
 		$this->lineaServicio = $lineaServicio;
 	}
-	public function consultarForSelect() {
+
+	public function consultar() {
 		$connClass = new Conexion ();
 		$conn = $connClass->getConection ();
 		if ($conn != null) {
-			if ($stmt = $conn->prepare ( "SELECT id, sigla FROM linea_servicio" )) {
+			if ($stmt = $conn->prepare ( "SELECT * FROM linea_servicio WHERE id=?" )) {
+				$id = $this->lineaServicio->getId ();
+				$stmt->bind_param ( "i", $id );
 				$stmt->execute ();
 				$result = $stmt->get_result();
 
-				$campos = array(0 => 'id',1 => 'sigla');
+				$campos = array(0 => 'id',1 => 'sigla',2 => 'descripcion');
 				return ConvertirJson::toJSON($campos,$result->fetch_all(MYSQLI_ASSOC));
 			}
 		}
 	}
+
+
+	public function guardar() {
+		$connClass = new Conexion ();
+		$conn = $connClass->getConection ();
+		if ($conn != null) {
+			if ($stmt = $conn->prepare ( "INSERT INTO linea_servicio (sigla, descripcion) VALUES (?,?)" )) {
+				$stmt->bind_param ( "ss", $this->lineaServicio->getSigla (), $this->lineaServicio->getDescripcion () );
+				$stmt->execute ();
+				if ($stmt->affected_rows > 0) {
+					return $stmt->insert_id;
+				} else {
+					return - 1;
+				}
+			}
+		}
+	}
+	public function modificar() {
+		$connClass = new Conexion ();
+		$conn = $connClass->getConection ();
+		if ($conn != null) {
+			if ($stmt = $conn->prepare ( "UPDATE linea_servicio SET sigla=?, descripcion=? WHERE id=?" )) {
+				$stmt->bind_param ( "ssi", $this->lineaServicio->getSigla (), $this->lineaServicio->getDescripcion (), $this->lineaServicio->getId ());
+				$stmt->execute ();
+				return $stmt->affected_rows;
+			}
+		}
+	}
 }
+
+
+
+
+
+
+
 ?>
