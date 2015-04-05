fbc10ab6b29a3aca037a97ba2137dcc25a8f3ffb
gabojrm <gabojrm@gmail.com>
2015-04-05 10:44:01 -0500

    Se actualiza lds controller

--- Malla_Turnos/CLASES/CONTROLLERS/ControllerLineaServicio.php
+++ Malla_Turnos/CLASES/CONTROLLERS/ControllerLineaServicio.php
@@ -4,41 +4,38 @@ include_once '../DAOS/DaoLineaServicio.php';
 
 header ( 'Content-type: application/json; charset=utf-8' );
 if (isset ( $_GET )) {
-	if (isset ($_GET['forselect'])) {
-		$lineaServicio = new LineaServicio();
-		$dao = new DaoLineaServicio($lineaServicio);
-		echo json_encode($dao->consultarForSelect());
+	if (isset ( $_GET ['forselect'] )) {
+		$lineaServicio = new LineaServicio ();
+		$dao = new DaoLineaServicio ( $lineaServicio );
+		echo json_encode ( $dao->consultarForSelect () );
 	}
 } else {
-// 	if (isset ( $_POST ['btnGuardar'] )) {
-// 		$res = array (
-// 				'resultado' => '',
-// 				'id' => - 1 
-// 		);
+	if (isset ( $_POST ['btnGuardar'] )) {
+		$res = array (
+				'resultado' => '',
+				'id' => - 1 
+		);
 		
-// 		$cliente = new Cliente ();
+		$lds = new LineaServicio ();
+		$lds->setSigla ( $_POST ['txtNombre'] );
+		$lds->setDescripcion ( $_POST ['txtDescrip'] );
 		
-// 		$cliente->setNombre ( $_POST ['txtNombre'] );
-// 		$cliente->setNit ( $_POST ['txtNit'] );
-// 		$cliente->setIdLineaServicio ( $_POST ['selLinServ'] );
-// 		$cliente->setActivo ( 1 );
+		$dao = new DaoLineaServicio ( $lds );
 		
-// 		$dao = new DaoCliente ( $cliente );
+		if (isset ( $_POST ['txtId'] )) {
+			$cliente->setId ( $_POST ['txtId'] );
+			$id = $dao->modificar ();
+		} else {
+			$id = $dao->guardar ();
+		}
+		if ($id < 0) {
+			$res ['resultado'] = 'Error';
+		} else {
+			$res ['id'] = $id;
+		}
 		
-// 		if (isset ( $_POST ['txtId'] )) {
-// 			$cliente->setId ( $_POST ['txtId'] );
-// 			$id = $dao->modificar ();
-// 		} else {
-// 			$id = $dao->guardar ();
-// 		}
-// 		if ($id < 0) {
-// 			$res ['resultado'] = 'Error';
-// 		} else {
-// 			$res ['id'] = $id;
-// 		}
-		
-// 		echo json_encode ( $res );
-// 	} else if (isset ( $_POST ['btnConsultar'] )) {
-// 	}
+		echo json_encode ( $res );
+	} else if (isset ( $_POST ['btnConsultar'] )) {
+	}
 }
 ?>
