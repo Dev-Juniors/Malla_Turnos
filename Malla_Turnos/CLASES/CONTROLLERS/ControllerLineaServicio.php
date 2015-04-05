eaacd8e9e6f11c69ec197b4704058d5c3ae2f63b
gabojrm <gabojrm@gmail.com>
2015-04-05 11:16:10 -0500

    Se actualiza controller lds

--- Malla_Turnos/CLASES/CONTROLLERS/ControllerLineaServicio.php
+++ Malla_Turnos/CLASES/CONTROLLERS/ControllerLineaServicio.php
@@ -3,20 +3,8 @@ include_once '../ENTITIES/LineaServicio.php';
 include_once '../DAOS/DaoLineaServicio.php';
 
 header ( 'Content-type: application/json; charset=utf-8' );
-if (isset ( $_GET )) {
-	if (isset ( $_GET ['forselect'] )) {
-		$lineaServicio = new LineaServicio ();
-		$dao = new DaoLineaServicio ( $lineaServicio );
-		echo json_encode ( $dao->consultarForSelect () );
-	}
-}
 
 if (isset ( $_POST ['btnGuardar'] )) {
-	$res = array (
-			'resultado' => '',
-			'id' => - 1 
-	);
-	
 	$lds = new LineaServicio ();
 	$lds->setSigla ( $_POST ['txtNombre'] );
 	$lds->setDescripcion ( $_POST ['txtDescrip'] );
@@ -24,18 +12,13 @@ if (isset ( $_POST ['btnGuardar'] )) {
 	$dao = new DaoLineaServicio ( $lds );
 	
 	if (isset ( $_POST ['txtId'] )) {
-		$cliente->setId ( $_POST ['txtId'] );
+		$lds->setId ( $_POST ['txtId'] );
 		$id = $dao->modificar ();
 	} else {
 		$id = $dao->guardar ();
 	}
-	if ($id < 0) {
-		$res ['resultado'] = 'Error';
-	} else {
-		$res ['id'] = $id;
-	}
 	
-	echo json_encode ( $res );
+	echo $id;
 } else if (isset ( $_POST ['btnConsultar'] )) {
 }
 
