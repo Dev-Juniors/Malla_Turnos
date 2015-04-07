1a32a6101ce30b71d824c6c6db4ac5e84fadae87
gabojrm <gabojrm@gmail.com>
2015-04-07 11:06:15 -0500

    Se actualiza

--- Malla_Turnos/index.php
+++ Malla_Turnos/index.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="expires" content="0">
 <link href="IMAGES/favicon.ico" rel="shortcut icon"
-	type="image/vnd.microsoft.icon">
+    type="image/vnd.microsoft.icon">
 <link rel="stylesheet" href="STYLES/bootstrap.min.css" />
 <link rel="stylesheet" href="STYLES/bootstrap-theme.min.css" />
 <link rel="stylesheet" href="STYLES/header.css" />
@@ -16,47 +16,46 @@
 <title>Malla de Turnos - Flag Soluciones</title>
 </head>
 <body>
-	<div id="headerDiv" class="row show-grid">
-		<div class="col-xs-6 col-sm-2">
-			<div id="logo">
-				<a href="menu.php"><img src="IMAGES/logo.png"></img></a>
-			</div>
-		</div>
-		<div class="col-xs-6 col-sm-10">
-			<h2 class="title">
-				<strong>Malla de turnos</strong>
-			</h2>
-		</div>
-	</div>
+    <div id="headerDiv" class="row show-grid">
+        <div class="col-sm-2">
+            <div id="logo">
+                <a href="index.php"><img src="IMAGES/logo.png"></img></a>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <h2 class="title">
+                <strong>Malla de turnos</strong>
+            </h2>
+        </div>
+    </div>
 
-	<div class="row show-grid">
-		<div class="col-xs-6 col-sm-2">
-			<div id="menu">
-				<ul class="nav nav-stacked">
+    <div class="row show-grid">
+        <div class="col-lg-2">
+            <div id="menu">
+                <ul class="nav nav-stacked">
 
-					<li class="active"><a href="#"><i class="glyphicon glyphicon-user"></i>
-							Gestionar Usuarios</a></li>
-					<li><a target="contenido" href="PAGES/GenerarModelo.php"><i
-							class="glyphicon glyphicon-calendar"></i> Generar Modelo</a></li>
-					<li class="nav-header"><a href="#" data-toggle="collapse"
-						data-target="#config"> Configuración <i
-							class="glyphicon glyphicon-chevron-right"></i></a>
-						<ul class="nav nav-stacked collapse" id="config">
-							<li><a target="contenido" href="PAGES/LineaServicio.php"><i
-									class="glyphicon glyphicon-menu-hamburger"></i> Línea de
-									Servicio</a></li>
-							<li><a target="contenido" href="PAGES/Cliente.php"><i
-									class="glyphicon glyphicon-globe"></i> Clientes</a></li>
-						</ul></li>
-				</ul>
-			</div>
-			<!-- /Main -->
-		</div>
-
-		<div class="col-xs-6 col-sm-8">
-			<iframe id="contenido" name="contenido"></iframe>
-		</div>
-	</div>
+                    <li class="active"><a href="#"><i class="glyphicon glyphicon-user"></i>
+                            Gestionar Usuarios</a></li>
+                    <li><a target="contenido" href="PAGES/GenerarModelo.php"><i
+                            class="glyphicon glyphicon-calendar"></i> Generar Modelo</a></li>
+                    <li class="nav-header"><a href="#" data-toggle="collapse"
+                        data-target="#config"> Configuración <i
+                            class="glyphicon glyphicon-chevron-right"></i></a>
+                        <ul class="nav nav-stacked collapse" id="config">
+                            <li><a target="contenido" href="PAGES/LineaServicio.php"><i
+                                    class="glyphicon glyphicon-menu-hamburger"></i> Línea de
+                                    Servicio</a></li>
+                            <li><a target="contenido" href="PAGES/Cliente.php"><i
+                                    class="glyphicon glyphicon-globe"></i> Clientes</a></li>
+                        </ul></li>
+                </ul>
+            </div>
+            <!-- /Main -->
+        </div>
+        <div class="col-lg-8">
+            <iframe id="contenido" name="contenido"></iframe>
+        </div>
+    </div>
 </body>
 <script type="text/javascript" src="SCRIPTS/menu.js"></script>
 </html>
